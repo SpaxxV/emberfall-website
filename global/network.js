883a92a5fc28ea0651bab59e6d8001cf380cac92
SpaxxV <100527358+SpaxxV@users.noreply.github.com>
2026-04-10 10:07:55 -0700

    Networking Patch

--- global/network.js
+++ global/network.js
@@ -1,19 +1,24 @@
-const doRedirects = true;
 const serverAddress = "https://api.emberfallevents.com/";
-const loadLocalStorageFromURL = false;
 async function externalGetRequest(url, callback) {
     const response = await fetch(url, {
         method : "GET"
     });
-    callback(await response.text());
+    callback(JSON.parse(await response.text()));
 }
 async function postRequest(endpoint, body, callback) {
     const response = await fetch(serverAddress + endpoint, {
         method : "POST",
-        body : body,
+        body : JSON.stringify(body),
         headers: {
             "Content-Type": "application/json",
         }
+    });
+    callback(JSON.parse(await response.text()));
+}
+async function internalGetRequest(endpoint, headers, callback) {
+    const response = await fetch(serverAddress + endpoint, {
+        method : "GET",
+        headers: headers
     });
     callback(await response.text());
 }
