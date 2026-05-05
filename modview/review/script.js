96135ba8fe3ab8818385c10bdec0dcba9bf70c02
SpaxxV <100527358+SpaxxV@users.noreply.github.com>
2026-05-04 17:22:57 -0700

    Quick asset update

---
+++ modview/review/script.js
@@ -0,0 +1,37 @@
+const params = new URLSearchParams();
+const querystring = new URLSearchParams(window.location.search).toString();
+let target = "modview/event/"
+if (querystring.length > 0) target += "?" + querystring;
+params.append("r", target);
+
+if (!localStorage.getItem("key")) {
+    window.location.replace("https://www.emberfallevents.com/modview/login/?" + params.toString());
+} else {
+    postRequest("modview/keystatus/", {
+        key: localStorage.getItem("key"),
+    }, (res) => {
+        if (!res.success) {
+            window.location.replace("https://www.emberfallevents.com/modview/login/?" + params.toString());
+        } else {
+            downloadModviewJS();
+        }
+    });
+}
+
+function downloadModviewJS() {
+    internalGetRequest("restricted/scripts/review.html", {
+        apikey: localStorage.getItem("key")
+    }, (res) => {
+        document.getElementById("bodyElem").innerHTML = res;
+
+        internalGetRequest("restricted/scripts/review.js?t=" + Date.now(), {
+            apikey: localStorage.getItem("key")
+        }, (res) => {
+            const script = document.createElement("script");
+            script.type = "text/javascript";
+            script.textContent = res;
+
+            document.head.appendChild(script);
+        });
+    });
+}
